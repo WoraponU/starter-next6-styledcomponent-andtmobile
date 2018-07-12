7eb05d0a3d029be2b051b2bd7f4f0ab21d4e900e
worapon prathuangthip <woraponok@gmail.com>
2018-07-12 15:17:11 +0700

    ใส่ andt mobile แบบง่าย

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React, { Component, Fragment } from 'react'
+import { Button } from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
 
 class Home extends Component {
   render() {
-    return <Fragment>test</Fragment>
+    return (
+      <Fragment>
+        test<Button />
+      </Fragment>
+    )
   }
 }
 
